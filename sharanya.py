a08ee5fcea7bb4df395d82077f72c6444acfc484
sharanya4002 <109149230+sharanya4002@users.noreply.github.com>
2022-07-19 21:40:10 +0530

    Add files via upload

---
+++ sharanya.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 19 21:27:36 2022
+
+@author: Sys177
+"""
+
+from math import pi
+r=float(input("input the radius of the circle:"))
+print("the area of the circle with radius "+str(r)+" is:"+str(pi*r**2))
+
