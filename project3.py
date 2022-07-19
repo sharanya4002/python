426c32038f8fd471f89140d771d25b98d6dfe4ea
sharanya4002 <109149230+sharanya4002@users.noreply.github.com>
2022-07-19 22:12:15 +0530

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 19 22:11:05 2022
+
+@author: Sys177
+"""
+
+fn = input("enter filename: ")
+f = fn.split(".")
+print("extension of the file is:" +f[-1])
